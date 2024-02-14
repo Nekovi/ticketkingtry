09359d1e35a2d0bf4fcf3437bc4bcf7c4c1e1ee0
Nekovi <57327193+Nekovi@users.noreply.github.com>
2024-02-14 16:39:57 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+document.getElementById('login').addEventListener('click',function(){
+
+document.querySelector('.bg-modal').style.display='flex';
+});
+
+
+
+document.querySelector('.close').addEventListener('click',function(){
+
+document.querySelector('.bg-modal').style.display="none";
+});
+
+
+document.getElementsByClassName('.login').addEventListener('click',function(){
+
+    document.querySelector('.bg-modal').style.display='flex';
+    });
+    
